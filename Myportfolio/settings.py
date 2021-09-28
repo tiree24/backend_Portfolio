9d61ac7485cc030d3bd3f79519c1988caa12ee88
Tiree Jackson <tiree_jackson@yahoo.com>
2021-09-28 17:23:02 -0400

    just added the models for myuser and did the forms for authapp

--- Myportfolio/settings.py
+++ Myportfolio/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+AUTH_USER_MODEL = "user_app.MyUser"
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Myportfolio/static'),)
--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
+
+class MyUser(AbstractUser):
+    following = models.ManyToManyField(
+        'self', related_name='follow', blank=True)
+    avatar = models.ImageField(upload_to='static/i', null=True, blank=True)
+    bio = models.CharField(max_length=500, null=True, blank=True)
+    slug = models.SlugField(unique=True, max_length=100, null=True, blank=True)
+    tags = TaggableManager()
